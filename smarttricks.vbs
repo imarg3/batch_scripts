09cbb5900f4da09b87cc187a2453fc26682f4f4d
gupta.arpit03 <gupta.arpit03@gmail.com>
2019-02-14 18:32:11 +0530

    added scripts

---
+++ smarttricks.vbs
@@ -0,0 +1,37 @@
+set wshshell = wscript.CreateObject("wScript.Shell")
+wshshell.run "Notepad"
+wscript.sleep 400
+wshshell.sendkeys "C"
+wscript.sleep 100
+wshshell.sendkeys "A"
+wscript.sleep 120
+wshshell.sendkeys "N"
+wscript.sleep 200
+wshshell.sendkeys "T"
+wscript.sleep 100
+wshshell.sendkeys "E"
+wscript.sleep 140
+wshshell.sendkeys "E"
+wscript.sleep 100
+wshshell.sendkeys "N"
+wscript.sleep 50
+wshshell.sendkeys " "
+wscript.sleep 120
+wshshell.sendkeys "A"
+wscript.sleep 170
+wshshell.sendkeys "A"
+wscript.sleep 100
+wshshell.sendkeys " "
+wscript.sleep 50
+wshshell.sendkeys "J"
+wscript.sleep 120
+wshshell.sendkeys "A"
+wscript.sleep 160
+wshshell.sendkeys "N"
+wscript.sleep 160
+wshshell.sendkeys "A"
+wscript.sleep 160
+wshshell.sendkeys " "
+wscript.sleep 200
+wshshell.sendkeys " "
+wscript.sleep 100
---
+++ windows_commands.bat
@@ -0,0 +1,93 @@
+@ECHO OFF
+CLS
+
+:: ANCHOR
+:COMMANDS
+
+ECHO *******************************************************************************
+ECHO AUTHOR - ARPIT GUPTA
+ECHO GROUP - GIST
+ECHO EMAIL ID - imarg3@github.com
+ECHO Logged-in-Username is %USERNAME% on %DATE% at %TIME%
+ECHO *******************************************************************************
+ECHO.
+color 0A
+REM Using this Batch Script, we can access Windows Commands
+ECHO BATCH SCRIPT FOR ACCESSING WINDOWS COMMANDS
+ECHO.
+ECHO.
+ECHO A. Windows Version and Architecture Type
+ECHO.
+ECHO B. Check IP Addresses Configuration
+ECHO.
+ECHO C. Check Windows Processes
+ECHO.
+ECHO D. Check Windows Port Status
+ECHO.
+ECHO E. DNS Lookup
+ECHO.
+ECHO F. PING MACHINE
+ECHO.
+ECHO G. Exit from Command Prompt
+ECHO.
+
+
+CHOICE /C ABCDEFG /N /M "Dear Arpit, Please Enter your choice:"
+
+:: Note - list ERRORLEVELS in decreasing order
+IF ERRORLEVEL 7 GOTO Exit
+IF ERRORLEVEL 6 GOTO PING
+IF ERRORLEVEL 5 GOTO NSLookup
+IF ERRORLEVEL 4 GOTO PortStatus
+IF ERRORLEVEL 3 GOTO QProcess
+IF ERRORLEVEL 2 GOTO IPAddress
+IF ERRORLEVEL 1 GOTO OS
+
+:OS
+CLS
+wmic os get caption && wmic os get osarchitecture
+ECHO.
+ECHO.
+GOTO COMMANDS
+
+:IPAddress
+CLS
+ipconfig /all
+ECHO.
+ECHO.
+GOTO COMMANDS
+
+:QProcess
+CLS
+qprocess
+ECHO.
+ECHO.
+GOTO COMMANDS
+
+:PortStatus
+CLS
+set /p PORT="Enter Port Number: "
+if %PORT% LEQ 0 (echo PORT Number is Invalid) else (netstat -nao | findstr %PORT%)
+ECHO.
+ECHO.
+GOTO COMMANDS
+
+:NSLookup
+CLS
+set /p IP="Enter IP ADDRESS / HOSTNAME TO LOOKUP: "
+nslookup %IP%
+ECHO.
+ECHO.
+GOTO COMMANDS
+
+:PING
+CLS
+set /p IP="Enter IP ADDRESS / HOSTNAME TO PING: "
+ECHO Press Ctrl + C to stop ping
+ping %IP% -t
+ECHO.
+ECHO.
+GOTO COMMANDS
+
+:Exit
+exit
